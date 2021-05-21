6de40ca8b8ff7b3b548e972a5a4a8407054ad8bf
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2021-05-21 10:27:32 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+a = [10, 20, -30, 44.54, "hello", 'abc']
+
+print(a[0]) # to access loction of characters in string
+print(a[1:4]) # var_name[starting_index, ending_index]
+print(a[1:]) # it will print everything after [1]
+print(a[-1]) # it will print last index of string
+for i in range(5):
+   print(a)
+
+
+
+
+
