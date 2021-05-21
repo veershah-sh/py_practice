91c11031fd3a298285ccbfa970138e464eb44efd
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2021-05-21 10:34:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
-a = [10, 20, -30, 44.54, "hello", 'abc']
+a = (10, 20, -30, 44.54, "hello", 'abc')
 
 print(a[0]) # to access loction of characters in string
 print(a[1:4]) # var_name[starting_index, ending_index]
 print(a[1:]) # it will print everything after [1]
 print(a[-1]) # it will print last index of string
-for i in range(5):
-   print(a)
+a[2] = -50
+# Traceback (most recent call last):
+#   File "C:\Users\S7r337 H4ck3r\Desktop\python\practice\test.py", line 8, in <module>
+#     a[2] = -50
+# TypeError: 'tuple' object does not support item assignment
 
+print(a)
 
 
 
