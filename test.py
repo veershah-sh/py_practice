690e8dfde26f40fe7d2f6cc8e5295b13e400e9fe
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2021-05-21 11:34:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
-lst = [1, 3, 4, 5, 2]  # this is a set
-s = set(lst)
+map_py = {1:"veer", 2:"rutvik", 3:"meet"}
 
-s.update([7, 8]) # to add
-s.remove(3) # to remove
-print(len(s)) # to get length of set
-print(type(s)) # to get type of set
+# map is a pair of key:value
+# {key1:value1, key2:value2, ..., keyn:valuen}
 
+d = {}
 
+d[10] = "rutvik"
+d[20] = "veer"
+print(d) # to print dictionary
+
+print(d.get(10))  # to get values from dictionary using key
+print(d.keys())   # to get keys of dictionary
+print(d.values()) # to get values of dictionary
+del d[20] # to delete item from dictionary
+d[10] = "meet" # to modify item of dictionary
+print(d)
 
 
