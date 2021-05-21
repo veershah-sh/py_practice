a9621b876742712bb2f9eba85b5a8f22d02eb72b
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2021-05-21 11:50:10 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,13 @@
-map_py = {1:"veer", 2:"rutvik", 3:"meet"}
+# escape sequence characters of string
 
-# map is a pair of key:value
-# {key1:value1, key2:value2, ..., keyn:valuen}
-
-d = {}
-
-d[10] = "rutvik"
-d[20] = "veer"
-print(d) # to print dictionary
-
-print(d.get(10))  # to get values from dictionary using key
-print(d.keys())   # to get keys of dictionary
-print(d.values()) # to get values of dictionary
-del d[20] # to delete item from dictionary
-d[10] = "meet" # to modify item of dictionary
-print(d)
+# \'	Single Quote	
+# \\	Backslash	
+# \n	New Line	
+# \r	Carriage Return	
+# \t	Tab	
+# \b	Backspace	
+# \f	Form Feed	
+# \ooo	Octal value	
+# \xhh	Hex value
 
 
